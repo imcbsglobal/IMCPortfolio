fc92739e007e84b1152239ece192d49e95615047
amalkrishna0123 <amalikka0@gmail.com>
2024-07-30 15:21:31 +0530

    Gap Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
     <div className=' md:ml-[300px] lg:ml-[400px] mt-5 p-5 md:p-5'>
       <section className=' Mlg:max-w-[1200px] Mlg:mx-auto'>
           <div className=' h-[100vh] w-full'>
-            <div className=' w-full mb-10 md:mb-0 grid place-items-center gap-10 HomeBanner'>
+            <div className=' w-full mb-0 md:mb-0 grid place-items-center gap-10 HomeBanner'>
               {/* Left */}
-              <div className=' mb-10'>
+              <div className=' mb-2'>
                 <div className="FontStyle-Top text-3xl md:text-[42px] text-[#363636] mb-5 leading-normal">Transforming <span className=' Mlg:block'>Ideas into</span> <span className='Mlg:block'>Impactful <span className=' text-[#f80]'>Designs</span></span>
                 </div>
                 <div className=' text-sm mb-5 GlassBg2 rounded-3xl'>A versatile management platform designed to provide comprehensive software solutions, including business software, Android/iOS development, web development, hardware and IT support, and business execution support. Our primary goal is to elevate the diverse products of RITS Software to the global market.</div>
@@ -28,7 +28,7 @@ const Home = () => {
             {/* Bottom Designs */}
            
            <div className='grid grid-cols-1 md:grid-cols-2 place-items-start Mlg:place-content-start Mlg:place-items-start'>
-              <div className=' h-[300px] w-auto flex justify-start items-center gap-2 mb-10'>
+              <div className=' h-[300px] w-auto flex justify-center xlg:justify-start items-center gap-2 mb-10'>
                 <div className=' h-[300px] w-[60%] border rounded-3xl shadow-2xl '>
                   <img src={Objectpng} className=' w-full h-full object-cover drop-shadow-2xl' alt="" />
                 </div>
