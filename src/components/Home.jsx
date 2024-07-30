8e925158615c42a0c0420a6851a0279fc096f3de
amalkrishna0123 <amalikka0@gmail.com>
2024-07-30 19:12:21 +0530

    Animation Started

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import homeBanner from "../assets/home-banner.jpg"
 import orange from "../assets/orange-shade.png"
 import homeBanner1 from "../assets/home-banner1.png"
 import Objectpng from "../assets/home-object1.png"
+import {motion} from "framer-motion"
 
 const Home = () => {
   return (
@@ -20,38 +21,63 @@ const Home = () => {
                 </div>
               </div>
               {/* Right */}
-              <div className='w-[320px] Responssive-Point xlg:w-auto max-h-[500px] h-full'>
-                <img src={homeBanner1} className=' drop-shadow-lg md:h-full md:w-full md:object-cover md:mt-[-40px]' alt="" />
-              </div>
+              <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 1, ease: 'easeInOut' }}
+              className='w-[320px] Responssive-Point xlg:w-auto max-h-[500px] h-full'
+            >
+              <img src={homeBanner1} className=' drop-shadow-lg md:h-full md:w-full md:object-cover md:mt-[-40px]' alt="home-banner" />
+            </motion.div>
             </div>
 
             {/* Bottom Designs */}
            
            <div className='grid grid-cols-1 md:grid-cols-2 place-items-center Mlg:place-content-start Mlg:place-items-start'>
               <div className=' h-[300px] w-auto flex justify-center xlg:justify-start items-center gap-2 mb-10'>
-                <div className=' h-[300px] w-[60%] border rounded-3xl shadow-2xl '>
+                <motion.div
+                initial={{y:100,opacity:0}}
+                animate={{y:0,opacity:1}}
+                transition={{duration:1.5,delay:0.5,ease:'backInOut'}}
+                className=' h-[300px] w-[60%] border rounded-3xl shadow-2xl '>
                   <img src={Objectpng} className=' w-full h-full object-cover drop-shadow-2xl' alt="" />
-                </div>
+                </motion.div>
                 <div className=' flex flex-col justify-center items-center gap-2'>
-                  <div className=' w-[100px] h-[145px] bg-black rounded-xl text-[#fff] p-5 text-center text-xl ObjectGradient2 font-bold'>
+                  <motion.div
+                  initial={{y:-100,opacity:0}}
+                  animate={{y:0,opacity:1}}
+                  transition={{duration:1.5,ease:'backInOut'}}
+                  className=' w-[100px] h-[145px] bg-black rounded-xl text-[#fff] p-5 text-center text-xl ObjectGradient2 font-bold'>
                     What We Do?
-                  </div>
-                  <div className=' w-[100px] h-[145px] bg-black rounded-xl overflow-hidden text-[#7e7e7e] text-[8px] text-center p-5 ObjectGradient3 md:p-3 md:text-[10px]'>
+                  </motion.div>
+                  <motion.div
+                  initial={{x:-100,opacity:0}}
+                  animate={{x:0,opacity:1}}
+                  transition={{duration:1.5,ease:'backInOut'}}
+                  className=' w-[100px] h-[145px] bg-black rounded-xl overflow-hidden text-[#7e7e7e] text-[8px] text-center p-5 ObjectGradient3 md:p-3 md:text-[10px]'>
                   a platform delivering comprehensive software solutions to elevate RITS Software globally.
-                  </div>
+                  </motion.div>
                 </div>
               </div>
 
               <div className=' flex flex-col justify-start items-center'>
-                <div className=' h-[150px] p-5 mb-5 rounded-3xl w-full ObjectGradient4 overflow-hidden'>
+                <motion.div
+                initial={{y:-100,opacity:0}}
+                animate={{y:0,opacity:1}}
+                transition={{duration:1.5,ease:'backInOut'}}
+                className=' h-[150px] p-5 mb-5 rounded-3xl w-full ObjectGradient4 overflow-hidden'>
                   <div className='mb-1 md:mb-0'><button className=' px-8 py-2 font-bold bg-[#fff] cursor-none rounded-3xl shadow-lg text-sm'>About Us</button></div>
                   <div className=' text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas tenetur assumenda beatae necessitatibus ea saepe dolore magnam natus et doloribus!</div>
-                </div>
+                </motion.div>
 
-                <div className=' h-[150px] p-5 mb-5 rounded-3xl w-full ObjectGradient4 overflow-hidden'>
+                <motion.div
+                initial={{y:100,opacity:0}}
+                animate={{y:0,opacity:1}}
+                transition={{duration:1.5,ease:'backInOut'}}
+                className=' h-[150px] p-5 mb-5 rounded-3xl w-full ObjectGradient4 overflow-hidden'>
                   <div className='mb-1 md:mb-0'><button className=' px-8 py-2 font-bold bg-[#fff] cursor-none rounded-3xl shadow-lg text-sm'>About Us</button></div>
                   <div className=' text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas tenetur assumenda beatae necessitatibus ea saepe dolore magnam natus et doloribus!</div>
-                </div>
+                </motion.div>
               </div>
 
            </div>
