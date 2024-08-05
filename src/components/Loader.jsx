93109e7d646fd6a9c5315d420cccdc9d5185d4f0
amalkrishna0123 <amalikka0@gmail.com>
2024-08-05 23:09:22 +0530

    Animate Deleted

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import Lottie from 'react-lottie';
-import animationData from '../Animation.json';
+
 
 const Loader = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-
   return (
     <div className="flex justify-center items-center h-full">
-      <Lottie options={defaultOptions} height={150} width={150} />
+      <h4>Loading.....</h4>
     </div>
   );
 };
